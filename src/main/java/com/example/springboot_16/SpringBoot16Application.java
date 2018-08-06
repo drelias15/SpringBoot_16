0225a5aecd9f2d0c7c00fcfeb6d0e06e72bbc13c
drelias15 <drelias15@gmail.com>
2018-08-06 15:33:37 -0400

    first commit

---
+++ src/main/java/com/example/springboot_16/SpringBoot16Application.java
@@ -0,0 +1,12 @@
+package com.example.springboot_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot16Application.class, args);
+	}
+}
